ffad3ae0056d7aa5f3beb57d2f81b23a30d5b88f
Мамаев Фёдор <fedor_mamaev_v@mail.ru>
2021-10-20 12:56:58 +0300

    Самостоятельная работа 20.10

--- src/main/java/com/fedormamaevv/SpringProject1/ApiController.java
+++ src/main/java/com/fedormamaevv/SpringProject1/ApiController.java
@@ -73,4 +73,122 @@ public class ApiController {
 
     // -----
 
+    @GetMapping("topics/comments-by-user/{username}")
+    public List<Comment> getCommentsByUser(@PathVariable("username") String username)
+    {
+        List<Comment> commentsByUser = new ArrayList<>();
+        for (Topic topic: topics)
+        {
+            for (Comment comment: topic.getComments())
+            {
+                if (comment.username.compareTo(username) == 0)
+                    commentsByUser.add(comment);
+            }
+        }
+        return commentsByUser;
+    }
+
+    @GetMapping("topics/{tIndex}/comments-by-user/{username}")
+    public List<Comment> getCommentsByUser(@PathVariable("tIndex") Integer topicIndex, @PathVariable("username") String username)
+    {
+        List<Comment> commentsByUser = new ArrayList<>();
+        for (Comment comment: topics.get(topicIndex).getComments())
+        {
+            if (comment.username.compareTo(username) == 0)
+                commentsByUser.add(comment);
+        }
+        return commentsByUser;
+    }
+
+    @DeleteMapping("topics/comments-by-user/{username}")
+    public void deleteCommentsByUser(@PathVariable("username") String username)
+    {
+        for (int t = 0; t < topics.size(); t++)
+        {
+            Topic topic = topics.get(t);
+            for (int c = 0; c < topic.getComments().size(); c++)
+            {
+                if (topic.getComments().get(c).username.compareTo(username) == 0)
+                {
+                    topic.deleteComment(c);
+                }
+            }
+            topics.set(t, topic);
+        }
+    }
+
+    @DeleteMapping("topics//{tIndex}/comments-by-user/{username}")
+    public void deleteCommentsByUser(@PathVariable("tIndex") Integer topicIndex, @PathVariable("username") String username)
+    {
+        Topic topic = topics.get(topicIndex);
+        for (int c = 0; c < topic.getComments().size(); c++)
+        {
+            if (topic.getComments().get(c).username.compareTo(username) == 0)
+            {
+                topic.deleteComment(c);
+            }
+        }
+        topics.set(topicIndex, topic);
+    }
+
+    @PutMapping("topics/{tIndex}/comments-by-user/{username}")
+    public void updateAllUserComments(@PathVariable("tIndex") Integer topicIndex, @PathVariable("username") String username, @RequestBody Comment comment)
+    {
+        Topic topic = topics.get(topicIndex);
+        for (int c = 0; c < topic.getComments().size(); c++)
+        {
+            if (topic.getComments().get(c).username.compareTo(username) == 0)
+            {
+                topic.updateComment(c, comment);
+            }
+        }
+        topics.set(topicIndex, topic);
+    }
+
+    @PutMapping("topics/{tIndex}/comments-by-user/{username}/{cIndex}")
+    public void updateUserComment(@PathVariable("tIndex") Integer topicIndex, @PathVariable("username") String username, @PathVariable("cIndex") Integer commentIndex, @RequestBody Comment comment)
+    {
+        Topic topic = topics.get(topicIndex);
+        int _commentIndex = commentIndex;
+        for (int c = 0; c < topic.getComments().size(); c++)
+        {
+            if (topic.getComments().get(c).username.compareTo(username) == 0)
+            {
+                if (_commentIndex == 0) {
+                    topic.updateComment(c, comment);
+                    topics.set(topicIndex, topic);
+                    return;
+                }
+                _commentIndex--;
+            }
+        }
+        topics.set(topicIndex, topic);
+    }
+
+    @GetMapping("topics/{tIndex}/comments-by-user/{username}/count")
+    public Integer getUserCommentCount(@PathVariable("tIndex") Integer topicIndex, @PathVariable("username") String username)
+    {
+        Topic topic = topics.get(topicIndex);
+        int count = 0;
+        for (int c = 0; c < topic.getComments().size(); c++)
+        {
+            if (topic.getComments().get(c).username.compareTo(username) == 0) count++;
+        }
+        return count;
+    }
+
+    @PutMapping("topics/{tIndex}/comments-by-user/{username}/by-content/{content}")
+    public void updateUserCommentByContent(@PathVariable("tIndex") Integer topicIndex, @PathVariable("username") String username, @PathVariable("content") String content, @RequestBody Comment comment)
+    {
+        Topic topic = topics.get(topicIndex);
+        for (int c = 0; c < topic.getComments().size(); c++)
+        {
+            if (topic.getComments().get(c).username.compareTo(username) == 0)
+            {
+                if (topic.getComments().get(c).text.indexOf(content) != -1)
+                    topic.updateComment(c, comment);
+            }
+        }
+        topics.set(topicIndex, topic);
+    }
 }
